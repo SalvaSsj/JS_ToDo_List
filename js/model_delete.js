0d3ae5d14d08f07b5dd5c37e049053474ca6bdcf
VanessaFAG <vanessaprincss.arreo@gmail.com>
2025-10-25 15:14:15 -0600

    Model y View Delete

--- js/model_delete.js
+++ js/model_delete.js
@@ -1,5 +1,25 @@
-delete(id){
-    const index = this.models.findIndex(model => model.id === id);
-    this.models.splice(index, 1);
-    this.save();
+export default class DeleteModel {
+
+    constructor(models, save) {
+        this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+    }
+    save() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+
+    findTodo(id) {
+        return this.todos.findIndex((todo) => todo.id === id);
+    }
+
+    removeTodo(id) {
+        const index = this.findTodo(id);
+        if (index > -1) {
+            this.todos.splice(index, 1);
+            this.save();
+        }
+        return this.todos;
+    }
+    setTodos(todos) {
+        this.todos = todos;
+    }
 }
